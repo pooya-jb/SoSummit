7e950879429fa544ff4b98e9d948221802410db8
loic khodarkovsky <lkhodarkovsky@gmail.com>
2024-01-29 18:53:11 +0100

    add chat component and logic:

--- client-mobile/components/ChatScreen.tsx
+++ client-mobile/components/ChatScreen.tsx
@@ -1,7 +1,64 @@
-import { Text } from "react-native";
+import { useState, useCallback } from "react";
+import { View, StyleSheet, Keyboard, TouchableWithoutFeedback } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
-export default function ChatScreen () {
-  return (
-    <Text>Hello from chat screen</Text>
-  )
+interface Message {
+  _id: number;
+  text: string;
+  createdAt: Date;
+  user: {
+    _id: number;
+    name: string;
+    avatar: string;
+  };
 }
+
+export default function ChatScreen() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  // useEffect(() => {
+  //   setMessages([
+  //     {
+  //       _id: 1,
+  //       text: 'Hello!',
+  //       createdAt: new Date(),
+  //       user: {
+  //         _id: 2,
+  //         name: 'React Native',
+  //         avatar: 'https://placeimg.com/140/140/any',
+  //       },
+  //     },
+  //   ]);
+  // }, []);
+
+  const onSend = useCallback((newMessages = []) => {
+    setMessages((prevMessages) => GiftedChat.append(prevMessages, newMessages));
+  }, []);
+
+  function handleTapOutside() {
+    Keyboard.dismiss();
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={handleTapOutside}>
+      <View style={styles.container}>
+          <GiftedChat
+            messages={messages}
+            onSend={(newMessages) => onSend(newMessages)}
+            user={{
+              _id: 1,
+            }}
+            placeholder="Send us any other information:"
+          />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    alignSelf: "center",
+  },
+});
