dc48cbd0b8e9ea64b4ba2dee362e28fd66fc41e4
gerysc04 <gerardsoclu@gmail.com>
2024-01-29 20:28:42 +0100

    feat(client-dashboard): add routes to frontend

--- client-dashboard/src/App.tsx
+++ client-dashboard/src/App.tsx
@@ -3,15 +3,22 @@ import * as React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Dashboard from './components/Dashboard/Dashboard';
 import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { RootState } from './redux/store';
 
 function App(): React.ReactNode {
-
+  const isAuthenticated = useSelector((state: RootState) => state.user.isAuthenticated)
   return (
     <>
       <ChakraProvider>
         <div id='app'>
           <Navbar />
-          <Dashboard />
+          <BrowserRouter >
+          <Routes >
+          <Route path="/" element={isAuthenticated?  <Dashboard />: <Dashboard />} />
+          </ Routes>
+          </BrowserRouter>
         </div>
       </ChakraProvider>
     </>
--- client-dashboard/src/components/Navbar/ChakraMenu/ChakraMenu.tsx
+++ client-dashboard/src/components/Navbar/ChakraMenu/ChakraMenu.tsx
@@ -5,10 +5,12 @@ import {
   MenuList,
   MenuItem,
 } from '@chakra-ui/react'
-
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../redux/store';
 // as = { Button } rightIcon = {< ChevronDownIcon />
 
 function ChakraMenu(): React.ReactNode {
+  const isAuthenticated = useSelector((state: RootState) => state.user.isAuthenticated)
 
   return (
     <>
@@ -16,13 +18,17 @@ function ChakraMenu(): React.ReactNode {
         <MenuButton color="var(--textmain)" bg="var(--backgroundmain)" h="100%" pl="1rem" pr="1rem">
           User
         </MenuButton>
-        <MenuList color="var(--textmain)" bg="var(--backgroundmain)" zIndex='999'>
-          <MenuItem color="var(--textmain)" bg="var(--backgroundmain)">Login</MenuItem>
-          <MenuItem color="var(--textmain)" bg="var(--backgroundmain)">Create a Copy</MenuItem>
-          <MenuItem color="var(--textmain)" bg="var(--backgroundmain)">Mark as Draft</MenuItem>
-          <MenuItem color="var(--textmain)" bg="var(--backgroundmain)">Delete</MenuItem>
-          <MenuItem color="var(--textmain)" bg="var(--backgroundmain)">Attend a Workshop</MenuItem>
+        { isAuthenticated? 
+          <MenuList color="var(--textmain)" bg="var(--backgroundmain)" zIndex='999'>
+          <MenuItem color="var(--textmain)" bg="var(--backgroundmain)">Settings</MenuItem>
+          <MenuItem color="var(--textmain)" bg="var(--backgroundmain)">Account settings</MenuItem>
         </MenuList>
+        :
+        <MenuList color="var(--textmain)" bg="var(--backgroundmain)" zIndex='999'>
+          <MenuItem color="var(--textmain)" bg="var(--backgroundmain)">Login as an admin</MenuItem>
+          <MenuItem color="var(--textmain)" bg="var(--backgroundmain)">Register as an admin</MenuItem>
+        </MenuList>
+        }
       </Menu>
     </>
   )
