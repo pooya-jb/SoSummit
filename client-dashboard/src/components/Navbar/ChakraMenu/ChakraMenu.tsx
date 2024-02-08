bc93496138f24fbc70fba07bf7c7fc7716f588c9
Giovanni Silvotti <giopr.silvotti@yahoo.com>
2024-02-08 12:13:35 +0100

    feat(client-dashboard): add newLocation Registration on dashboard and few fixes

--- client-dashboard/src/components/Navbar/ChakraMenu/ChakraMenu.tsx
+++ client-dashboard/src/components/Navbar/ChakraMenu/ChakraMenu.tsx
@@ -9,12 +9,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../../redux/store';
 
 import LoginModal from '../../ChakraModals/LoginModal';
-import { loginSelected, registerSelected } from '../../../redux/displaySlice';
+import { locationRegisterSelected, loginSelected, registerSelected } from '../../../redux/displaySlice';
 import RegisterModal from '../../ChakraModals/RegisterModal';
 import { loggedOut } from '../../../redux/userSlice';
 import JWTUtil from '../../../utils/jwtUtil';
 import AlertModal from '../../ChakraModals/AlertModal';
 import './ChakraMenu.css'
+import LocationRegisterModal from '../../ChakraModals/newLocationModal';
 
 function ChakraMenu(): React.ReactNode {
   const isAuthenticated = useSelector((state: RootState) => state.user.isAuthenticated);
@@ -30,13 +31,16 @@ function ChakraMenu(): React.ReactNode {
     dispatch(registerSelected())
   }
 
+  function handleLocationRegisterClick() {
+    dispatch(locationRegisterSelected())
+  }
+
   function handleLogoutClick() {
     JWTUtil.destroyer()
     dispatch (loggedOut())
   }
 
   return (
-    <>
       <Menu>
         <MenuButton className="button" h="100%" pl="1rem" pr="1rem">
           {isAuthenticated ? `${username}` : "Ski Resort Login"}
@@ -49,14 +53,14 @@ function ChakraMenu(): React.ReactNode {
         :
         <MenuList className="menu-list" zIndex='999'>
             <MenuItem className="menu-item" onClick={handleLoginClick}>Login</MenuItem>
+            <MenuItem className="menu-item" onClick={handleLocationRegisterClick}>Register a New Location</MenuItem>
         </MenuList>
         }
         {<LoginModal/>}
         {<RegisterModal />}
         {<AlertModal />}
+        {<LocationRegisterModal />}
       </Menu>
-
-    </>
   )
 }
 
